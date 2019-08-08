b9d8dfebb60edf1edb5cea69b2342d2fe01e79c2
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-08-08 14:34:13 +0530

    Create Concept_class.java

---
+++ Concept_class.java
@@ -0,0 +1,16 @@
+class Concept_class{
+//member section
+int a,b,c;
+//__________________________________________
+//method section
+void add(){
+a=10;
+b=10;
+c=a+b;
+System.out.println(c);
+}
+public static void main (String[] fdfd){
+Concept_class obj1=new Concept_class();
+obj1.add();
+}
+}
