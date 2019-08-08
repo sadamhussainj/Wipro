5b5ff3b940dc041a618b4a9e1fd15bd972f0b5b9
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-08-08 14:34:57 +0530

    Create UserMainCode.java

---
+++ UserMainCode.java
@@ -0,0 +1,14 @@
+class UserMainCode{
+int isEven(int input1){
+if(input1%2==0)
+return 2;
+else
+return 1;
+}
+public static void main(String[] sdfd){
+UserMainCode obj=new UserMainCode();
+int x=100,y=0;
+y=obj.isEven(x);
+System.out.println(y);
+}
+}
