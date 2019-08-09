4032fe9dea844b1c24499fe33f9c0a9663b4f628
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-08-09 13:29:00 +0530

    Create GetArray.java

---
+++ GetArray.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class GetArray{
+public static void main(String args[]){
+int arr[]=new int[10];
+for(int i=0;i<10;i=i+1){
+arr[i]=i;
+}
+for(int i=0;i<10;i=i+1){
+System.out.println(arr[i]);
+}
+}
+}
