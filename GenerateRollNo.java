0aacebdaae6718168a7aa587febf7f446218c4dc
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-08-15 14:52:03 +0530

    Update GenerateRollNo.java

--- GenerateRollNo.java
+++ GenerateRollNo.java
@@ -5,12 +5,14 @@ Scanner scan=new Scanner(System.in);
 System.out.print("Enter the Name");
 String Name=scan.nextLine();
 System.out.print("Enter the DOB");
-String DOB=scan.next();
+String DOB=scan.nextLine();
 System.out.print("Enter the Dept");
-String Dept=scan.next();
+String Dept=scan.nextLine();
 System.out.print("Enter the College");
 String College=scan.nextLine();
-String Regno=College.substring(0,1)+Dept.substring(0,2)+DOB.substring(0,2)+Name.substring(0,4)+DOB.substring(2,4)+DOB.substring(3,5);
-System.out.print(Regno);
+String RollNo=College.substring(0,1)+Dept.substring(0,2)+DOB.substring
+
+(0,2)+Name.substring(0,4)+DOB.substring(3,5)+DOB.substring(6,8);
+System.out.print("RollNo"+is RollNo);
 }
 }
