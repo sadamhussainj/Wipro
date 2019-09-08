d44e427173c526148d636acaaa6110b9bf94b5b4
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-09-08 13:22:47 +0530

    Update and rename StringUppLow.java to UppLowString.java

---
+++ UppLowString.java
@@ -0,0 +1,10 @@
+public class UppLowString{
+
+     public static void main(String []args){
+         String s1="Hello";
+         String s2=s1.toLowerCase();
+         String s3=s1.toUpperCase();
+         System.out.println(s4);
+         
+     }
+}
