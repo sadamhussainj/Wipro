84eaf59b1517834fa8c7d6ce72a33b6973db9226
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-09-08 12:28:37 +0530

    Create StringSplit.java

---
+++ StringSplit.java
@@ -0,0 +1,11 @@
+public class StringSplit{
+
+     public static void main(String []args){
+         String s1="Hello";
+         for(int i=0;i<s1.length();i++)
+         {
+         char ch=s1.charAt(i);
+        System.out.println(ch);
+         }
+     }
+}
