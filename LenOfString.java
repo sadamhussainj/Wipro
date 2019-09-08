659c14da3f507c8ab5460a4e2d53da83aa8c6901
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-09-08 13:26:55 +0530

    Create LenOfString.java

---
+++ LenOfString.java
@@ -0,0 +1,9 @@
+public class LenOfString{
+
+     public static void main(String []args){
+         String s1="sadam hussain";
+         int n=s1.length();
+         System.out.println(n);
+         
+     }
+}
