43b8ce6477e6e41d5e4f361594b0439e1aa1967c
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-09-08 15:18:31 +0530

    Create DecimaltoBinary.java

---
+++ DecimaltoBinary.java
@@ -0,0 +1,12 @@
+//usingfunction
+class DecimaltoBinary{
+     
+    public static void main(String a[]){
+    	System.out.println("Binary representation of 124: ");
+    	System.out.println(Integer.toBinaryString(124));
+        System.out.println("\nBinary representation of 45: ");
+        System.out.println(Integer.toBinaryString(45));
+        System.out.println("\nBinary representation of 999: ");
+        System.out.println(Integer.toBinaryString(999));
+    }
+}
